9de66cace4273c7752a1dcecc33270c0801c353b
Magnus Holm <judofyr@gmail.com>
2013-01-30 09:52:54 +0100

    Use a proper example

--- ex.pl
+++ ex.pl
@@ -5,7 +5,11 @@ plugin 'Devlicious';
 
 get '/' => sub {
   my $self = shift;
-  $self->ua->get('http://bbc.co.uk/', sub { });
+  $self->render_later;
+
+  $self->ua->get('http://www.bbc.co.uk/', sub {
+    $self->render_text(pop->res->dom->at('title')->text);
+  });
 };
 
 app->start;
